b74072ee709b0070069efe13fcfdcc4b59d05df8
Emerson Duvan Vasquez Monteros <70560@bi.com.gt>
2025-09-30 16:34:53 -0600

    Siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/EmersonV00/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	Estado, Texto := variables.ConviertoaTexto(1588)
+	fmt.Println(Estado)
+	fmt.Println(Texto)
 }
